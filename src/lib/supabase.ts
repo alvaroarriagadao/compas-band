bee137c149f6df480fe0d325d36ed21e99df8bd6
AlvaroArriagadaCB <alvaroarriagada@currencybird.cl>
2026-06-03 18:33:24 -0400

    chore: migrate to new Supabase account (alvaro.arriagada101)

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 import type { Database } from './database.types'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://odhrurajnwphfelwjhze.supabase.co'
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9kaHJ1cmFqbndwaGZlbHdqaHplIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA1MTQ5MDcsImV4cCI6MjA5NjA5MDkwN30._Ohmljkm4xUmuDiHNJbsYmbbsJjGRHzvV9fEPPBbZfs'
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://dkxwxgupswhpnyegkogc.supabase.co'
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRreHd4Z3Vwc3docG55ZWdrb2djIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA1MTg2MzIsImV4cCI6MjA5NjA5NDYzMn0.cfn_NAXKO_BCGuUdMhZpAnl87cvcNdO9FeXZNW10uKw'
 
 export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey)
